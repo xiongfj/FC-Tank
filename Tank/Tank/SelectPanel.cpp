956f3b253a1fe939b0776f862f6d9fc10307c7af
xiongfj <837943056@qq.com>
2017-07-23 08:28:47 +0800

    modified SelectPanel.cpp

--- Tank/Tank/SelectPanel.cpp
+++ Tank/Tank/SelectPanel.cpp
@@ -6,6 +6,22 @@ SelectPanel::SelectPanel( HDC des_hdc, HDC image_hdc)
 	//mGraphics = grap;
 	mDes_hdc = des_hdc;
 	mImage_hdc = image_hdc;
+	loadimage(&mSelect_player_image, _T("./res/big/select_player.gif"));
+
+	// 选择坦克手柄游标
+	loadimage(&mSelectTankImage[0], _T("./res/big/0Player/m0-2-1.gif"));
+	loadimage(&mSelectTankImage[1], _T("./res/big/0Player/m0-2-2.gif"));
+	// 灰色背景
+	loadimage(&mGrayBackgroundImage, _T("./res/big/bg_gray.gif"));
+	loadimage(&mBlackNumberImage, _T("./res/big/black-number.gif"));
+
+	mSelectTankPoint[0].x = 60;
+	mSelectTankPoint[1].x = 60;
+	mSelectTankPoint[2].x = 60;
+	mSelectTankPoint[0].y = 123;
+	mSelectTankPoint[1].y = 139;
+	mSelectTankPoint[2].y = 156;
+
 	Init();
 }
 
@@ -15,34 +31,10 @@ SelectPanel::~SelectPanel()
 
 void SelectPanel::Init()
 {
-	// 背景选择图片
 	mSelect_player_image_y = WINDOW_HEIGHT;
-	//mSelect_player_image = Image::FromFile( L"./res/big/select_player.gif" );
-	loadimage(&mSelect_player_image, _T("./res/big/select_player.gif"));
 
-	// 选择坦克手柄游标
-	//mSelectTankImage[0] = Image::FromFile( L"./res/big/0Player/m0-2-1.gif" );
-	//mSelectTankImage[1] = Image::FromFile( L"./res/big/0Player/m0-2-2.gif" );
-	loadimage(&mSelectTankImage[0], _T("./res/big/0Player/m0-2-1.gif"));
-	loadimage(&mSelectTankImage[1], _T("./res/big/0Player/m0-2-2.gif"));
-
-	mSelectTankPoint[0].x = 60;
-	mSelectTankPoint[1].x = 60;
-	mSelectTankPoint[2].x = 60;
-	mSelectTankPoint[0].y = 123;
-	mSelectTankPoint[1].y = 139;
-	mSelectTankPoint[2].y = 156;
 	mSelectIndex = 1;
 	mCounter = 1;
-
-	// 灰色背景
-	//mGrayBackgroundImage = Image::FromFile(L"./res/big/bg_gray.gif");
-	loadimage(&mGrayBackgroundImage, _T("./res/big/bg_gray.gif"));
-	// STAGE 字样
-	//mCurrentStageImage = Image::FromFile(L"./res/big/stage.gif");
-	//loadimage(&mCurrentStageImage, _T("./res/big/stage.gif"));
-	// 黑色 1234567890 数字
-	loadimage(&mBlackNumberImage, _T("./res/big/black-number.gif"));
 }
 
 //
@@ -60,16 +52,12 @@ EnumSelectResult SelectPanel::ShowSelectPanel()
 			mSelect_player_image_y = 0;
 
 		// 绘制在 mImage_hdc 上
-		//mGraphics->DrawImage(mSelect_player_image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT );
 		TransparentBlt(mImage_hdc, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT, 
 			GetImageHDC(&mSelect_player_image), 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT, 0x000000);
 		
 		// 将 mImage_hdc 绘制到主窗口 mDes_hdc 上
 		StretchBlt(mDes_hdc, 0, mSelect_player_image_y, WINDOW_WIDTH, WINDOW_HEIGHT, mImage_hdc, 0, 0 , CANVAS_WIDTH, CANVAS_HEIGHT, SRCCOPY);
 		FlushBatchDraw();
-
-		//if ( mSelect_player_image_y == 0 )
-		//	break; 
 	}
 
 	int temp = 0;					// 控制按键响应速度,不能太快!
@@ -78,10 +66,8 @@ EnumSelectResult SelectPanel::ShowSelectPanel()
 	{
 		Sleep(40);
 		mCounter++;
-		//mGraphics->DrawImage(mSelect_player_image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT );
 		BitBlt(mImage_hdc, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT, GetImageHDC(&mSelect_player_image), 0, 0, SRCCOPY);
 
-		//mGraphics->DrawImage(mSelectTankImage[mCounter], mSelectTankPoint[mSelectIndex].x, mSelectTankPoint[mSelectIndex].y , 16, 16 );
 		TransparentBlt(mImage_hdc, mSelectTankPoint[mSelectIndex].x, mSelectTankPoint[mSelectIndex].y, 16, 16,
 			GetImageHDC(&mSelectTankImage[mCounter]), 0, 0, 16, 16, 0x000000);
 
@@ -105,6 +91,9 @@ EnumSelectResult SelectPanel::ShowSelectPanel()
 			break;
 		}
 	}
+
+	// 重置数据, 下次才能上升
+	Init();
 
 	// 响应玩家的选择
 	switch( mSelectIndex )
