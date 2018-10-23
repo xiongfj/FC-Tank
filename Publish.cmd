a50b7b92e12a4c1ba9156b4bdf0b751d73034f7e
xiongfj <xiongfj@easyx.cn>
2018-10-23 19:04:04 +0800

    [Assist] 添加发布 cmd 脚本

---
+++ Publish.cmd
@@ -0,0 +1,9 @@
+﻿
+
+:: 同步到 Publish 文件夹
+"D:\App_Portable\SyncFile\SyncFile.exe" /srcdir:"E:\Work\FC-Tank\Tank\Tank\res" /moddir:"E:\Work\FC-Tank\Publish.modify" /dstdir:"E:\Work\FC-Tank\Publish\res" -fixsrcmodtime:true
+
+
+copy E:\Work\FC-Tank\Tank\x64\Release\Tank.exe E:\Work\FC-Tank\Publish\Tank.exe
+
+pause
